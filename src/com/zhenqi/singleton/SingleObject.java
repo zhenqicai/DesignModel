a3c1162bb5ff93d8a38db1732962003cf142c11e
zhenqicai <zhenqicai@sohu.com>
2017-02-22 21:49:00 -0600

    单例模式。

--- src/com/zhenqi/singleton/SingleObject.java
+++ src/com/zhenqi/singleton/SingleObject.java
@@ -4,4 +4,20 @@ package com.zhenqi.singleton;
  * Created by jo on 2017/2/22.
  */
 public class SingleObject {
+
+    //创建 SingleObject 的一个对象
+    private static SingleObject instance = new SingleObject();
+
+    //让构造函数为 private，这样该类就不会被实例化
+    private SingleObject(){}
+
+    //获取唯一可用的对象
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+
 }
